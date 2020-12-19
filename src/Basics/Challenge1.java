a9826a5b10d6ddaad54074447f3126570dc63293
jamesjames1 <james44nuguid@gmail.com>
2020-12-19 19:56:53 +0800

    Initial commit

---
+++ src/Basics/Challenge1.java
@@ -0,0 +1,32 @@
+package Basics;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        int position1 = calculateHighScorePosition(1500);
+        displayHighScorePosition("James", position1);
+        displayHighScorePosition("Jack", calculateHighScorePosition(900));
+        displayHighScorePosition("Peter", calculateHighScorePosition(400));
+        displayHighScorePosition("John", calculateHighScorePosition(50));
+
+    }
+
+    public static void displayHighScorePosition(String player, int position){
+        System.out.println(player + " managed to get into position " +
+                position + " on the highscore table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if (playerScore >= 1000){
+            return 1;
+        }
+        else if (playerScore >= 500){
+            return 2;
+        }
+        else if (playerScore >= 100){
+            return 3;
+        }
+        else {
+            return 4;
+        }
+    }
+}
