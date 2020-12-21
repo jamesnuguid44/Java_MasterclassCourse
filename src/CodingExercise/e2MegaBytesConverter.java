6e31402832ae2134ba5e4153aaa46fbff6b6b523
jamesjames1 <james44nuguid@gmail.com>
2020-12-21 11:45:00 +0800

    CodingExercise1

---
+++ src/CodingExercise/e2MegaBytesConverter.java
@@ -0,0 +1,17 @@
+package CodingExercise;
+
+public class e2MegaBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(-123);
+        printMegaBytesAndKiloBytes(7240);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + (kiloBytes / 1024) + " MB and " +
+                    (kiloBytes % 1024) + " KB");
+        }
+    }
+}
