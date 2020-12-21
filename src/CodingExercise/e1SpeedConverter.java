6d0b8f41b10b6d708148615f410c3e8c1b8dc7bb
jamesjames1 <james44nuguid@gmail.com>
2020-12-21 10:28:27 +0800

    CodingExercise1

---
+++ src/CodingExercise/e1SpeedConverter.java
@@ -0,0 +1,25 @@
+package CodingExercise;
+
+public class e1SpeedConverter {
+    public static void main(String[] args) {
+        System.out.println(toMilesPerHour(10.25));
+        printConversion(10.25);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour+ " km/h = " +
+                    toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+}
