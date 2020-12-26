5fc3708ee336619c1ef5137f678abf8caa2dbfe7
jamesjames1 <james44nuguid@gmail.com>
2020-12-26 23:41:28 +0800

    Exercise11

---
+++ src/Basics/C3SecondsAndMinutes.java
@@ -0,0 +1,47 @@
+package Basics;
+
+public class C3SecondsAndMinutes {
+    public static void main(String[] args) {
+        getDurationString(70,50);
+        getDurationString(3690);
+    }
+
+    public static void getDurationString(int minutes, int seconds){
+        if (minutes<0 || (seconds<0 || seconds>59)){
+            System.out.println("Invalid Value");
+        } else {
+            int hour = minutes/60;
+            int remainingMinutes = minutes%60;
+            
+            if (hour<10){
+                System.out.print("0"+hour+"h ");
+            } else {
+                System.out.print(hour+"h ");
+            }
+            
+            if (remainingMinutes<10){
+                System.out.print("0"+remainingMinutes+"m ");
+            } else {
+                System.out.print(remainingMinutes+"m ");
+            }
+
+            if (seconds<10){
+                System.out.println("0"+seconds+"s ");
+            } else {
+                System.out.println(seconds+"s ");
+            }
+
+            //System.out.println(hour+"h "+remainingMinutes+"m "+seconds+"s");
+        }
+    }
+
+    public static void getDurationString(int seconds){
+        if (seconds<0){
+            System.out.println("Invalid Value");
+        } else {
+            int minutes = seconds/60;
+            int remainingSeconds = seconds%60;
+            getDurationString(minutes,remainingSeconds);
+        }
+    }
+}
---
+++ src/CodingExercise/e11PlayingCat.java
@@ -0,0 +1,19 @@
+package CodingExercise;
+
+public class e11PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (summer && (temperature>24 && temperature<46)){
+            return true;
+        } else if (temperature>24 && temperature<36){
+            return true;
+        }
+            return false;
+
+    }
+}
